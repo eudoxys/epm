f2abeba1f9295fdaa4328711b4a6fff8846d56d3
David P. Chassin <david.chassin@me.com>
2025-10-12 13:48:04 -0700

    Add catalog command

--- epm/__init__.py
+++ epm/__init__.py
@@ -6,6 +6,8 @@ Options:
 
     -h|--help: get this help
 
+    -v|--version: get the EPM version number
+
 Commands:
 
     help: get this help
@@ -15,12 +17,44 @@ Commands:
 
 import os
 import sys
+import re
+import importlib.metadata
+import warnings
+try:
+    from .catalog import Catalog
+except ImportError:
+    from catalog import Catalog
+try:
+    __version__ = importlib.metadata.version('epm')
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "dev"
+
+def list(pattern:str=None) -> list[list[str]]:
+
+    result = []
+    for package in importlib.metadata.distributions():
+        name = package.name
+        if not pattern or re.match(pattern,name):
+            info = importlib.metadata.distribution(package.name).metadata
+            try:
+                warnings.filterwarnings("error")
+                keywords = info["Keywords"]
+                warnings.resetwarnings()
+            except (KeyError, DeprecationWarning):
+                keywords = None
+            if "eudoxys" in str(keywords).split(","):
+                description = " / ".join([x for x in info['Description'].split("\n") if x and not x.startswith("#")])
+                version = info["Version"]
+                result.append([name,version,description])
+
+    return result
+
 
 def main(
-    args=sys.argv[1:],
-    stdout=lambda x: print(x,file=sys.stdout),
-    stderr=lambda x: print(x if x.startswith("Syntax: ") else f"ERROR [epm]: {x}",file=sys.stderr),
-    exceptions=None
+    args:list=sys.argv[1:],
+    stdout:callable=lambda x: print(x,file=sys.stdout),
+    stderr:callable=lambda x: print(x if x.startswith("Syntax: ") else f"ERROR [epm]: {x}",file=sys.stderr),
+    exceptions:list=None
     ):
 
     try:
@@ -33,15 +67,63 @@ def main(
             stderr("\n".join([x for x in __doc__.split("\n") if x.startswith("Syntax: ")]))
             return E_SYNTAX
 
+        #
+        # Options
+        #
+
         if args[0] in ["-h","--help","help"]:
 
             stdout(__doc__)
             return E_OK
 
+        if args[0] in ["-v","--version"]:
+
+            stdout(__version__)
+            return E_OK
+
+        #
+        # Commands
+        #
+
+        # list - get list of installed packages
         if args[0] == "list":
 
-            stdout("No packages installed")
+            stdout(list())
             return E_OK
+
+        # catalog - get list of available packages
+        if args[0] == "catalog":
+
+            catalog = Catalog()
+            if catalog.index:
+                result = {}
+                header = ["package","version","description"]
+                underline = {x:len(x) for x in header}
+                for package in catalog.index:
+                    info = Catalog().metadata(package)
+                    try:
+                        description = info["project"]["description"]
+                    except KeyError:
+                        description = ""
+                    try:
+                        version = info["version"]
+                    except KeyError:
+                        version = ""
+                    result[package] = {x:eval(x) for x in header}
+                    underline = {x:max(underline[x],len(eval(x))) for x in header}
+                print(" ".join([x.title()+" "*(underline[x]-len(x)) for x in header]))
+                print(" ".join(['-'*underline[x] for x in header]))
+                for package,info in result.items():
+                    print(" ".join([y+" "*(underline[x]-len(y)) for x,y in info.items()]))
+            return E_OK
+
+        # install - install packages
+
+        # uninstall - uninstall packages
+
+        # upgrade - upgrade packages
+
+        # open - open package webpage
 
         raise ValueError(f"'{args[0]}' is an invalid command")
 
@@ -51,3 +133,7 @@ def main(
             raise
         stderr(f"{e_type.__name__} {e_value}")
         return E_EXCEPTION
+
+if __name__ == "__main__":
+
+    main(['catalog'])
---
+++ epm/catalog.py
@@ -0,0 +1,53 @@
+"""Eudoxys package catalog"""
+
+import os
+import re
+import urllib.request
+import tomllib
+
+class Catalog:
+
+    REPO = "https://github.com/eudoxys"
+    DATA = "https://raw.githubusercontent.com/eudoxys"
+    LIST = [   
+        "epm",
+    ]
+
+    def __init__(self,pattern:str=None):
+        self.index = [x for x in self.LIST if not pattern or re.match(pattern,x)]
+
+    def repository(self,package):
+        assert package in self.index, f"{package} is not in catalog index"
+        return os.path.join(self.REPO,package)
+
+    def metadata(self,package):
+        assert package in self.index, f"{package} is not in catalog index"
+        toml = os.path.join(self.DATA,package,"refs","heads","main","pyproject.toml")
+        with urllib.request.urlopen(toml) as query:
+            return tomllib.loads(query.read().decode('utf-8'))
+
+if __name__ == "__main__":
+
+    import json
+
+    catalog = Catalog()
+    if catalog.index:
+        result = {}
+        header = ["package","version","description"]
+        underline = {x:len(x) for x in header}
+        for package in catalog.index:
+            info = Catalog().metadata(package)
+            try:
+                description = info["project"]["description"]
+            except KeyError:
+                description = ""
+            try:
+                version = info["version"]
+            except KeyError:
+                version = ""
+            result[package] = {x:eval(x) for x in header}
+            underline = {x:max(underline[x],len(eval(x))) for x in header}
+        print(" ".join([x.title()+" "*(underline[x]-len(x)) for x in header]))
+        print(" ".join(['-'*underline[x] for x in header]))
+        for package,info in result.items():
+            print(" ".join([y+" "*(underline[x]-len(y)) for x,y in info.items()]))
